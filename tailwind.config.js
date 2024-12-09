e4a789054c0a0a342c9b9bb11da9a8de016a0cf1
Marcus <marcus.veres@gmail.com>
2024-12-09 17:20:09 -0500

    initial design for the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'mono': ['Anonymous Pro', ...defaultTheme.fontFamily.mono],
+        'sans': ['Montserrat', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
